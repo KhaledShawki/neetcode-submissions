28e984bad4055ab8390bf79843eb2110409d943f
Khaled Shawki <52483377+KhaledShawki@users.noreply.github.com>
2026-07-04 23:55:51 +0200

    Add: insertionSort - submission-3

---
+++ Data Structures & Algorithms/insertionSort/submission-3.cpp
@@ -0,0 +1,32 @@
+// Definition for a Pair
+// class Pair {
+// public:
+//     int key;
+//     string value;
+//
+//     Pair(int key, string value) : key(key), value(value) {}
+// };
+class Solution 
+{
+public:
+    vector<vector<Pair>> insertionSort(vector<Pair>& pairs) 
+    {
+        vector<vector<Pair>> result;
+        
+        for (int i = 0; i < static_cast<int>(pairs.size()); ++i)
+        {
+            int j = i - 1;
+
+            while(j >= 0 && pairs[j + 1].key < pairs[j].key)
+            {
+                swap(pairs[j + 1], pairs[j]);
+                --j;
+            }
+
+            result.push_back(pairs);
+        }
+
+        return result;
+    
+    }
+};
